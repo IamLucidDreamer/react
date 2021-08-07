5c4e21803ad77b776d513ebf08dc3607f806d3e9
IamLucidDreamer <shuklamanasofficial@gmail.com>
2021-08-07 08:23:06 -0700

    Footer Fixed

--- src/MyComponents/Partials/Footer.js
+++ src/MyComponents/Partials/Footer.js
@@ -9,19 +9,19 @@ class Footer extends Component {
         super(props)
     
         this.state = {
-             email: ''
+             subscriber: ''
         }
     }
 
-    handleEmailChange = (event) => {
+    handleSubscriberChange = (event) => {
         this.setState({
-            email: event.target.value
+            subscriber: event.target.value
         })
 
     }
 
     handleSubmit = (event) => {
-        alert(`${this.state.email}`)
+        alert(`${this.state.subscriber}`)
         event.preventDefault()
     }
 
@@ -37,23 +37,17 @@ class Footer extends Component {
       <div class="lg:w-1/4 md:w-1/2 w-full px-4">
         <img class="w-24 items-center" src={logo} alt="Logo of Argus Security"/>
         <p class="text-white">Your protection is Our Mission. Proudly serving business and residential sector in Ontario.</p>
-        <hr class="border-1 border-gray-600 w-11/12 my-2 py-2" />
+        <hr class="border-1 border-gray-600 w-11/12 my-3" />
         <a href="tel:6472891070" class="p-1">
-            <div>
                 {/* <img src={phone_logo} alt="Phone Logo" /> */}
                 <p>647-289-1070</p>
-            </div>
         </a>
         <a href="mailto:info@argussecurityservices.ca" class="p-1">
-            <div>
                 {/* <img src={phone_logo} alt="Phone Logo" /> */}
                 <p>info@argussecurityservices.ca</p>
-            </div>
         </a>
-        <div class="p-1">
             {/* <img src={phone_logo} alt="Phone Logo" /> */}
             <p>350 Rutherford Road South Brampton ON L6W-4N6 Suite 210 Plaza 2</p>
-        </div>
     </div>
       <div class="lg:w-1/4 md:w-1/2 w-full px-4">
         <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">EXPLORE</h2>
@@ -119,18 +113,16 @@ class Footer extends Component {
           </li>
         </nav>
       </div>
-      <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">SUBSCRIBE</h2>
-        <div class="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-          <div class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-            <label for="footer-field" class="leading-7 text-sm text-gray-600">Placeholder</label>
-            <input type="text" id="footer-field" name="footer-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-          </div>
-          <button class="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+      <div class="lg:w-1/4 md:w-1/2 w-full px-4 text-center items-center">
+        <div class="flex flex-col bg-red-700 items-start p-6 ">
+          <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3 text-center">NEWSLETTER</h2>
+          <p class="text-white text-sm mt-2 text-center">Signup to get our daily latest
+          security news and updates.</p>
+          <form class="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+            <input type="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <button className="w-full p-4 rounded-lg border text-white bg-black hover:bg-white hover:text-red-700 hover:border-black">REGISTER NOW</button>
+          </form>
         </div>
-        <p class="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
-          <br class="lg:block hidden"/>waistcoat green juice
-        </p>
       </div>
     </div>
   </div>
